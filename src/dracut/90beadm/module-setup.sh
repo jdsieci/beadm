475a15ae704692ef98ddb47b1f487153f3877cc2
Jerzy Drozdz <jerzy.drozdz@gmail.com>
2018-08-30 18:53:21 +0200

    Added module setup for dracut

---
+++ src/dracut/90beadm/module-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+check() {
+  test -x /sbin/beadm || return 1
+  return 0
+}
+
+
+
+depends() {
+  echo zfs
+  return 0
+}
+
+install() {
+  inst "${moddir}/beadm.conf" "/etc/beadm.conf"
+}
